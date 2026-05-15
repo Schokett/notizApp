cecdfcf93ea24bbf47e6bef44581d7ee3ee50c4c
schokett <skrollanelenazimmermann@gmail.com>
2026-05-16 00:26:37 +0200

    get locastorage data

--- getNotes.js
+++ getNotes.js
@@ -3,10 +3,34 @@ const LOCAL_STORAGE_KEY = "notebook-notes";
 // Daten vom noteElement in den localStorage packen
 function saveDataLocalStorage() {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(note));
-  console.log(note);
+
   //   speichert nur einen!!!
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const noteContainerElement = document.getElementById("saved-note-container");
+  const saveData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  console.log(saveData);
+
+  const div = document.createElement("div");
+  const title = document.createElement("p");
+  const text = document.createElement("p");
+  const date = document.createElement("p");
+
+  div.classList.add("saved-note");
+  title.classList.add("title-history");
+  text.classList.add("text-history");
+  date.classList.add("date-history");
+
+  title.textContent = saveData.title;
+  text.textContent = saveData.text;
+  date.textContent = saveData.date;
+
+  div.appendChild(title);
+  div.appendChild(text);
+  div.appendChild(date);
+
+  if (noteContainerElement) {
+    noteContainerElement.appendChild(div);
+  }
 });
